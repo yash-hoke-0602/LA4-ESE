70cbcea62b30a0cb1b3bff75b46194c05fabfe00
yash-hoke-0602 <71776251+yash-hoke-0602@users.noreply.github.com>
2021-01-01 04:48:36 -0800

    Add files via upload

---
+++ Webpage/login.js
@@ -0,0 +1,28 @@
+function login()
+	{
+		var uname = document.getElementById("email").value;
+		var pwd = document.getElementById("pwd1").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='')
+		{
+			alert("please enter user name.");
+		}
+		else if(pwd=='')
+		{
+        	alert("enter the password");
+		}
+		else if(!filter.test(uname))
+		{
+			alert("Enter valid email id.");
+		}
+		else if(pwd.length < 6 )
+		{
+			alert("Password min 6.");
+		}
+		else
+		{
+	alert(uname+' \nThank You for Login');
+  //Redirecting to other page or webste code or you can set your own html page.
+       window.location = "index.html";
+			}
+	}
